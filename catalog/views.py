e6a06db8abffe5f9ec711cf60887591da9a41dad
vladimirbutyrskii <vlaver@mail.ru>
2026-01-22 21:32:03 +0300

    continue home_23; add controllers, views, templates for products

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'products_detail.html', context)
 
 
 def contacts(request):
