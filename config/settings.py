8d3a4687811b341e042d0a3194111be6c48fac8e
vladimirbutyrskii <vlaver@mail.ru>
2026-01-23 16:41:34 +0300

    add photos to media/ and inc_menu

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,6 @@ STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
