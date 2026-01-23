25eefecd8de4c1853ba949d59712b0bc81b2de6c
vladimirbutyrskii <vlaver@mail.ru>
2026-01-23 13:06:02 +0300

    continue home_23; extract common base template; add code to display product cards in a loop

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, products_detail
+from catalog.views import products_list, products_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", products_list, name='products_list'),
-    path("products/<int:pk>/", products_detail, name='products_detail'),
-    path('contacts/', contacts, name='contacts'),
+    path("products/<int:pk>/", products_detail, name='products_detail')
+
 ]
 
